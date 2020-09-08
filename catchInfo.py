7c9b7471872ab732bcd86276aadf142baa11f469
BGuy13 <bguy6013@gmail.com>
2020-09-08 09:05:07 +0800

    0907_update

--- catchInfo.py
+++ catchInfo.py
@@ -21,13 +21,13 @@ datas = cursor.fetchall()
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
 info = {}
 cntO = 0
-fp = io.open("check.json", "w", encoding="utf-8")
+fp = io.open("check.txt", "w", encoding="utf-8")
 for data in datas:
     res = rq.get(data[2], headers = headers)
     content = res.content.decode("utf-8")
-    info[cntO] = {} # 以下待修改
-    info['char_id'] = data[0]
-    info['charName'] = data[1]
+    sk0 = {}
+    sk0['char_id'] = data[0]
+    sk0['charName'] = data[1]
     # print(str(data[0]) + ":" + data[1])
     # fp.write(data[1] + "\n")
 
@@ -35,7 +35,6 @@ for data in datas:
     print('start')
 
     # 技能等級一到七
-    info[] = {}
     cnt = 1
     for i in range(2, 8):
         sk0[cnt] = {}
@@ -53,52 +52,52 @@ for data in datas:
     # print(sk0)
     fp.write(str(sk0) + "\n")
 
-    # # 技能等級八到十
-    # sk8_10 = {}
-    # cnto = 1
-    # for s in range(1, 4):
-    #     sk8_10[cnto] = {}
-    #     cnti = 7
-    #     for i in range(8, 11):
-    #         sk8_10[cnto][cnti] = {}
-    #         sk8_10[cnto][cnti]['level'] = i
-    #         sk8_10[cnto][cnti]['materials'] = {}
-    #         for j in range(1, 4):
-    #             try:
-    #                 sk8_10[cnto][cnti]['materials'][j] = {}
-    #                 sk8_10[cnto][cnti]['materials'][j]['name'] = cc.convert(html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(s+1) + ']/tbody/tr[5]/td[' + str(i-7) + ']/a[' + str(j) + ']/div/table/tr[1]/td[2]/text()')[0])
-    #                 sk8_10[cnto][cnti]['materials'][j]['quantity'] = html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(s+1) + ']/tbody/tr[5]/td[' + str(i-7) + ']/a[' + str(j) + ']/span/text()')[0]
-    #             except:
-    #                 del sk8_10[cnto][cnti]['materials'][j]
-    #                 break
-    #         cnti += 1
-    #     cnto += 1
-    # # print(sk8_10)
-    # fp.write(str(sk8_10) + "\n")
+    # 技能等級八到十
+    sk8_10 = {}
+    cnto = 1
+    for s in range(1, 4):
+        sk8_10[cnto] = {}
+        cnti = 7
+        for i in range(8, 11):
+            sk8_10[cnto][cnti] = {}
+            sk8_10[cnto][cnti]['level'] = i
+            sk8_10[cnto][cnti]['materials'] = {}
+            for j in range(1, 4):
+                try:
+                    sk8_10[cnto][cnti]['materials'][j] = {}
+                    sk8_10[cnto][cnti]['materials'][j]['name'] = cc.convert(html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(s+1) + ']/tbody/tr[5]/td[' + str(i-7) + ']/a[' + str(j) + ']/div/table/tr[1]/td[2]/text()')[0])
+                    sk8_10[cnto][cnti]['materials'][j]['quantity'] = html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(s+1) + ']/tbody/tr[5]/td[' + str(i-7) + ']/a[' + str(j) + ']/span/text()')[0]
+                except:
+                    del sk8_10[cnto][cnti]['materials'][j]
+                    break
+            cnti += 1
+        cnto += 1
+    # print(sk8_10)
+    fp.write(str(sk8_10) + "\n")
 
-    # # 菁英化
-    # elite = {}
-    # cnt = 1
-    # for i in range(1, 3):
-    #     elite[cnt] = {}
-    #     elite[cnt]['level'] = i
-    #     elite[cnt]['materials'] = {}
-    #     for j in range(1, 6):
-    #         try:
-    #             elite[cnt]['materials'][j] = {}
-    #             elite[cnt]['materials'][j]['name'] = cc.convert(html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(i+5) + ']/tbody/tr[4]/td/a[' + str(j) + ']/div/table/tr[1]/td[2]/text()')[0])
-    #             if not elite[cnt]['materials'][j]['name']:
-    #                 # print("in")
-    #                 del elite[cnt]['materials'][j]
-    #                 break
-    #             elite[cnt]['materials'][j]['quantity'] = html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(i+5) + ']/tbody/tr[4]/td/a[' + str(j) + ']/span/text()')
-    #         except:
-    #             del elite[cnt]['materials'][j]
-    #             break
-    #     cnt += 1
-    # # print(elite)
-    # fp.write(str(elite) + "\n")
-    # print('end')
+    # 菁英化
+    elite = {}
+    cnt = 1
+    for i in range(1, 3):
+        elite[cnt] = {}
+        elite[cnt]['level'] = i
+        elite[cnt]['materials'] = {}
+        for j in range(1, 6):
+            try:
+                elite[cnt]['materials'][j] = {}
+                elite[cnt]['materials'][j]['name'] = cc.convert(html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(i+5) + ']/tbody/tr[4]/td/a[' + str(j) + ']/div/table/tr[1]/td[2]/text()')[0])
+                if not elite[cnt]['materials'][j]['name']:
+                    # print("in")
+                    del elite[cnt]['materials'][j]
+                    break
+                elite[cnt]['materials'][j]['quantity'] = html.xpath('/html/body/div[2]/div[1]/div[2]/table[' + str(i+5) + ']/tbody/tr[4]/td/a[' + str(j) + ']/span/text()')
+            except:
+                del elite[cnt]['materials'][j]
+                break
+        cnt += 1
+    # print(elite)
+    fp.write(str(elite) + "\n")
+    print('end')
 
 print('everything_end')
 fp.close()
---
+++ catchInfoToJson.py
@@ -0,0 +1,88 @@
+# !/usr/bin/python
+# coding:utf-8
+
+import requests as rq
+from lxml import etree
+import io
+import json
+import mysql.connector
+from opencc import OpenCC
+cc = OpenCC('s2twp')
+
+arkdb = mysql.connector.connect(
+    host = "192.168.168.146",
+    user = "arkdeve",
+    password = "BGuy6013@",
+    database = "arknightDB"
+)
+cursor = arkdb.cursor()
+
+cursor.execute("SELECT char_id, charName, infoLink FROM charSummary")
+datas = cursor.fetchall()
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"}
+info = {}
+fp = io.open("check.json", "w", encoding="utf-8")
+for data in datas:
+    res = rq.get(data[2], headers = headers)
+    content = res.content.decode("utf-8")
+
+    info[data[0]] = {}
+    info[data[0]]["name"] = data[1]
+    info[data[0]]["demand"] = {}
+    de = info[data[0]]["demand"]
+
+    de["skill"] = {}
+    sk = de["skill"]
+
+    de["elite"] = {}
+    el = de["elite"]
+
+    html = etree.HTML(content)
+    print("start")
+
+    for i in range(2, 8):
+        sk["0_" + str(i)] = {}
+        sk["0_" + str(i)]["materials"] = {}
+        ski = sk["0_" + str(i)]["materials"]
+        for j in range(1, 4):
+            try:
+                ski["m" + str(j)] = {}
+                ski["m" + str(j)]["name"] = cc.convert(html.xpath("/html/body/div[2]/div[1]/div[2]/table[5]/tbody/tr[" + str(i-1) + "]/td/a[" + str(j) + "]/div/table/tr[1]/td[2]/text()")[0])
+                ski["m" + str(j)]["quantity"] = html.xpath("/html/body/div[2]/div[1]/div[2]/table[5]/tbody/tr[" + str(i-1) + "]/td/a[" + str(j) + "]/span/text()")[0]
+            except:
+                del ski["m" + str(j)]
+                break
+    # print(sk)
+
+    for s in range(1, 4):
+        for i in range(8, 11):
+            sk[str(s) + "_" + str(i)] = {}
+            sk[str(s) + "_" + str(i)]["materials"] = {}
+            sksi = sk[str(s) + "_" + str(i)]["materials"]
+            for j in range(1, 4):
+                try:
+                    sksi["m" + str(j)] = {}
+                    sksi["m" + str(j)]["name"] = cc.convert(html.xpath("/html/body/div[2]/div[1]/div[2]/table[" + str(s+1) + "]/tbody/tr[5]/td[" + str(i-7) + "]/a[" + str(j) + "]/div/table/tr[1]/td[2]/text()")[0])
+                    sksi["m" + str(j)]["quantity"] = html.xpath("/html/body/div[2]/div[1]/div[2]/table[" + str(s+1) + "]/tbody/tr[5]/td[" + str(i-7) + "]/a[" + str(j) + "]/span/text()")[0]
+                except:
+                    del sksi["m" + str(j)]
+                    break
+    
+    for i in range(1, 3):
+        el[str(i)] = {}
+        el[str(i)]["materials"] = {}
+        eli = el[str(i)]["materials"]
+        for j in range(1, 6):
+            try:
+                eli["m" + str(j)] = {}
+                eli["m" + str(j)]["name"] = cc.convert(html.xpath("/html/body/div[2]/div[1]/div[2]/table[" + str(i+5) + "]/tbody/tr[4]/td/a[" + str(j) + "]/div/table/tr[1]/td[2]/text()")[0])
+                if not eli["m" + str(j)]["name"]:
+                    del eli["m" + str(j)]
+                    break
+                eli["m" + str(j)]["quantity"] = html.xpath("/html/body/div[2]/div[1]/div[2]/table[" + str(i+5) + "]/tbody/tr[4]/td/a[" + str(j) + "]/span/text()")[0]
+            except:
+                del eli["m" + str(j)]
+                break
+        
+json.dump(info, fp, ensure_ascii=False)
+fp.close()
